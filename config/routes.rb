a04707ea76ac53045c0d79684f318d329e64ec91
ninachandra <nina.chandra@gmail.com>
2014-02-05 11:02:39 -0600

    hw week 4 - weather

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Week4hw::Application.routes.draw do
 
-  get "/" => "pages#home"
+  get "weather/search" => "weather#search"
+  get "weather/conditions" => "weather#conditions"
 
   # HINT: Add more routes here...
-
 
 
 end
